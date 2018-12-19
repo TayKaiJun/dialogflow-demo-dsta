ac05a54af1a10699d166cbe5958c5a5d5ae198ed
TayKaiJun <tehohno@gmail.com>
2018-12-19 10:34:53 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,9 +1,7 @@
 import flask
+
 app = flask.Flask(__name__)
 
 @app.route("/")
 def index():
     return "Hello Heruko"
-
-#if __name__ = '__main__':
-#	app.run(host = '127.0.0.1', port = 8080, debug = True)
