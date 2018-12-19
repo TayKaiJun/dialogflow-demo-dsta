2a81637d577ce2fc35ba9f4dcf3b1bfad3a8ccfd
TayKaiJun <tehohno@gmail.com>
2018-12-19 15:22:13 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,12 +1,25 @@
-import urllib
+import os
 import json
 
-import flask
+from flask import Flask
 from flask import request
 from flask import make_response
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Hello Heruko"
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+    print("Request:")
+    print(json.dumps(req, indent=4))
+    res = processRequest(req)
+    res = json.dumps(res, indent=4)
+    print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+def processRequest(req):
+    return {
+        "fulfillmentText": "Hello, you're in Heroku",
+    }
